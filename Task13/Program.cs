f62863c62b226da9ef50d606babba776e05d8a5f
sharlottakorde@gmail.com <sharlottakorde@gmail.com>
2022-11-23 14:21:05 +0300

    init

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или 
+сообщает, что третьей цифры нет.*/
+
+Console.Write("Enter a Number: ");
+int AnyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(AnyNumber);
+if (anyNumberText.Length > 2)
+{
+Console.WriteLine("Third digit: " + anyNumberText[2]);
+}
+else
+{
+  Console.WriteLine("No third digit");
+}

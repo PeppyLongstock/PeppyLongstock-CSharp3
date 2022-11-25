af01379f9932d6921f953106baa1a224f20b445b
sharlottakorde@gmail.com <sharlottakorde@gmail.com>
2022-11-25 16:56:43 +0300

    prepush

---
+++ Tasks3/Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+Console.WriteLine("Enter the Number"); 
+int Number = Convert.ToInt32(Console.ReadLine());
+int Squares = 1;
+while (Squares <= Number)
+{
+    Console.WriteLine(Squares * Squares * Squares + "");
+    Squares++;
+}
